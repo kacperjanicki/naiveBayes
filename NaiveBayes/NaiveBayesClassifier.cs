a685bc770cc7a7076f1fc4404fbb52d8b80e2d67
kacperjanicki <kacperekjanicki9@gmail.com>
2026-04-15 19:09:29 +0200

    calculating a priori and a posteriori probabilities

---
+++ NaiveBayes/NaiveBayesClassifier.cs
@@ -0,0 +1,107 @@
+﻿namespace NaiveBayes;
+
+public class NaiveBayesClassifier
+{
+    private bool _applySmoothingAll;
+    private Dictionary<string, double> _priorProbabilities;
+    private Dictionary<string, Dictionary<string, Dictionary<string, double>>> _conditionalProbabilities;
+    
+    public NaiveBayesClassifier(bool applySmoothingAll, List<Observation> trainDataset)
+    {
+        _applySmoothingAll = applySmoothingAll;
+        _conditionalProbabilities = new Dictionary<string, Dictionary<string, Dictionary<string, double>>>();
+        _priorProbabilities = new Dictionary<string, double>();
+        CalculatePriorProbabilities(trainDataset);
+        CalculateConditionalProbabilities(trainDataset);
+
+    }
+    
+    private void CalculatePriorProbabilities(List<Observation> dataset)
+    {
+        int allOccurences = dataset.Count;
+        var decisions = dataset.Select(o => o.Decision).Distinct();
+
+        foreach (var d in decisions)
+        {
+            _priorProbabilities[d] = 0;
+        }
+        
+        foreach (var observation in dataset)
+        {
+            _priorProbabilities[observation.Decision] += 1;
+        }
+
+        foreach (var pair in _priorProbabilities)
+        {
+            _priorProbabilities[pair.Key] = pair.Value / allOccurences;
+        }
+    }
+
+    private void CalculateConditionalProbabilities(List<Observation> dataset)
+    {
+        var featureNames = dataset.First().Features.Keys; // ["Nogi", "Jaja", ... ,]
+        var decisionValues = _priorProbabilities.Keys; // ["Tak, "Nie"]
+        
+        var uniqueValuesCount = featureNames.ToDictionary(
+            name => name,
+            name => dataset.Select(o => o.Features[name]).Distinct().Count()
+        );
+        
+        var allPossibleValues = featureNames.ToDictionary(
+            name => name,
+            name => dataset.Select(o => o.Features[name]).Distinct().ToList()
+        );
+        
+        foreach (var VARIABLE in allPossibleValues)
+        {
+            Console.WriteLine(string.Join(VARIABLE.Key, VARIABLE.Value));
+        }
+
+        foreach (var decision in decisionValues)
+        {
+            _conditionalProbabilities[decision] = new Dictionary<string, Dictionary<string, double>>();
+            var observationsInClass = dataset.Where(o => o.Decision == decision).ToList();
+            int denominator = observationsInClass.Count();
+
+            foreach (var feature in featureNames)
+            {
+                int classes = uniqueValuesCount[feature];
+                _conditionalProbabilities[decision][feature] = new Dictionary<string, double>();
+
+                foreach (var value in allPossibleValues[feature])
+                {
+                    int numerator = observationsInClass.Count(o => o.Features[feature] == value);
+                    double probability;
+
+                    if (_applySmoothingAll)
+                    {
+                        probability = SimpleSmoothing(numerator, denominator, classes);
+                    }
+                    else
+                    {
+                        if (numerator == 0)
+                        {
+                            probability = SimpleSmoothing(numerator, denominator, classes);
+                        }
+                        else
+                        {
+                            probability = (double)numerator / denominator;
+                        }
+                    }
+
+                    _conditionalProbabilities[decision][feature][value] = probability;
+                }
+            }
+
+        }
+
+        Console.WriteLine(decisionValues);
+    }
+
+    public double SimpleSmoothing(int numerator, int denominator, int classes)
+    {
+        return (double)(numerator + 1) / (denominator + classes);
+    }
+
+
+}
--- NaiveBayes/Program.cs
+++ NaiveBayes/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace NaiveBayes
+{
+    public class Observation
+    {
+        public Dictionary<string, string> Features { get; set; }
+        public string Decision { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Observation> allData = new List<Observation>
+{
+                new Observation { Decision = "no", Features = new Dictionary<string, string> { { "outlook", "sunny" }, { "temperature", "hot" }, { "humidity", "high" }, { "windy", "false" } } },
+                new Observation { Decision = "no", Features = new Dictionary<string, string> { { "outlook", "sunny" }, { "temperature", "hot" }, { "humidity", "high" }, { "windy", "true" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "overcast" }, { "temperature", "hot" }, { "humidity", "high" }, { "windy", "false" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "rainy" }, { "temperature", "mild" }, { "humidity", "high" }, { "windy", "false" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "rainy" }, { "temperature", "cool" }, { "humidity", "normal" }, { "windy", "false" } } },
+                new Observation { Decision = "no", Features = new Dictionary<string, string> { { "outlook", "rainy" }, { "temperature", "cool" }, { "humidity", "normal" }, { "windy", "true" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "overcast" }, { "temperature", "cool" }, { "humidity", "normal" }, { "windy", "true" } } },
+                new Observation { Decision = "no", Features = new Dictionary<string, string> { { "outlook", "sunny" }, { "temperature", "mild" }, { "humidity", "high" }, { "windy", "false" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "sunny" }, { "temperature", "cool" }, { "humidity", "normal" }, { "windy", "false" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "rainy" }, { "temperature", "mild" }, { "humidity", "normal" }, { "windy", "false" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "sunny" }, { "temperature", "mild" }, { "humidity", "normal" }, { "windy", "true" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "overcast" }, { "temperature", "mild" }, { "humidity", "high" }, { "windy", "true" } } },
+                new Observation { Decision = "yes", Features = new Dictionary<string, string> { { "outlook", "overcast" }, { "temperature", "hot" }, { "humidity", "normal" }, { "windy", "false" } } },
+                new Observation { Decision = "no", Features = new Dictionary<string, string> { { "outlook", "rainy" }, { "temperature", "mild" }, { "humidity", "high" }, { "windy", "true" } } }
+            };
+            int testSize = 2;
+            int trainSize = allData.Count - testSize;
+            List<Observation> trainDataset = allData.GetRange(0, trainSize);
+            List<Observation> testDataset = allData.GetRange(trainSize, testSize);
 
-Console.WriteLine("Hello, World!");
+            NaiveBayesClassifier classifier = new NaiveBayesClassifier(true,allData);
+
+
+        }
+    }
+}
